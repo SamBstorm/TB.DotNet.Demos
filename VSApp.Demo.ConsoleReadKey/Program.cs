c24701c4c26c1fc65352e1555a8c92ea14ae85d3
Legrain Samuel <samuel.legrain@bstorm.be>
2021-04-23 13:44:09 +0200

    Add Demo Console.readKey()

---
+++ VSApp.Demo.ConsoleReadKey/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace VSApp.Demo.ConsoleReadKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo cki = new ConsoleKeyInfo();
+            do
+            {
+                cki = Console.ReadKey();
+                Console.WriteLine($"Caractère : {cki.KeyChar}, Touche : {cki.Key} + {cki.Modifiers}");
+
+            } while (!(cki.Key == ConsoleKey.Q && cki.Modifiers == ConsoleModifiers.Control));
+        }
+
+
+        /*
+         Définissez un enum d'action ( Avancer, reculer, tourner à gauche, tourner à droite, valider, ouvrir inventaire, quitter,...)
+        Créez en une seul variable une liaison de touches aux actions définis dans notre énum de telle sorte que vous puissez afficher le nom des actions à la pression d'une touche.
+
+        Exemple :
+        J'appuie sur i => la Console affiche OuvrirInventaire
+        J'appuie sur e => la Console affiche Valider
+        J'appuie sur Enter => la Console affiche Valider
+        J'appuie sur z => la Console affiche Avancer
+        J'appuie sur Flèche du haut => la Console affiche Avancer
+        J'appuie sur 7 du NumPad => la Console affiche Avancer
+        ...
+         
+         */
+    }
+}
